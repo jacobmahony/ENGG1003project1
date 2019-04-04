a7b052ac8a58c86c18100d80bcf587c204ec3c4c
Jacob Mahony <c3330522@uon.edu.au>
2019-04-04 08:24:02 +0000

    stdin code via text from input

--- main.c
+++ main.c
@@ -2,11 +2,19 @@
 //ABCDEFGHIJKLMNOPQRSTUVWXYZ
 //ZABCDEFGHIJKLMNOPQRSTUVWXY
 
-encryption(int a)
-
+int e(int a); //encryption function prototype 
+char a[10][10];
 int main() {
-    
-    
-    
+    /*for(int i=0; i<26; i++) {
+        printf("%c %d\n", i+65, i+65);
+    }*/
+FILE *input;
+input = fopen("input", "r");
+fscanf(input, "%s %s %s", a[0],a[1],a[2]);
+printf("%s %s %s", a[0],a[1],a[2]);
     return 0;
 }
+
+/*int e(int a) { //encryption function definition
+
+}*/
