cad284723ef1183a970294ce60133c45b94a1d9b
Jacob Mahony <c3330522@uon.edu.au>
2019-04-04 08:54:48 +0000

    task 1 v1.1

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
-//ABCDEFGHIJKLMNOPQRSTUVWXYZ
-//ZABCDEFGHIJKLMNOPQRSTUVWXY
 
-int e(int a); //encryption function prototype 
-char a[10][10];
+
+#include <stdio.h>
+
 int main() {
-    /*for(int i=0; i<26; i++) {
-        printf("%c %d\n", i+65, i+65);
-    }*/
-FILE *input;
-input = fopen("input", "r");
-fscanf(input, "%s %s %s", a[0],a[1],a[2]);
-printf("%s %s %s", a[0],a[1],a[2]);
+    char message[100][100];
+    int i, key = 4;
+    FILE *input;
+    input = fopen("input", "r");
+    fscanf(input, "%s %s %s", message[1], message[2], message[3]);
+    printf(Original message: )
+    for(i = 0; message[i] != '\0'; i++) {
+        message[i] = message[i] + key;
+    }
+    printf("Encrypted message: %s", message);
     return 0;
 }
-
-/*int e(int a) { //encryption function definition
-
-}*/
