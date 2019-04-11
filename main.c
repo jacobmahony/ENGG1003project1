50c961b5c6c793982250d5387bec0b095dc762bd
Jacob Mahony <c3330522@uon.edu.au>
2019-04-11 08:58:15 +0000

    file reader v1.1

--- main.c
+++ main.c
@@ -1,19 +1,16 @@
-
-
 #include <stdio.h>
 
 int main() {
-    char message[100][100]; //creates a 100x100 array to store many strings
-    int i, j, key = 4;
-    FILE *input; //pointer to file name
-    input = fopen("input", "r"); //opens input for reading
-    fscanf(input, "%s %s %s", message[1], message[2], message[3]); //scans the 3 different words from input and stores them in the array
-    printf("Original message: %s %s %s\n", message[1], message[2], message[3] );
-    for(i = 1; i < 4; i++) { //only applies code to the first 3 messages in input
-        for(j = 0; message[i][j] != '\0'; j++) { //ends for loop when it reaches a null character
-            message[i][j] = message[i][j] + key; //accesses each character and shifts it by key
-        }        
+    char c;
+    FILE *input; //pointer to input
+    input = fopen("input", "r"); //reads input
+    if(input == NULL) { 
+        perror("fopen()"); //prints user-friendly error if error occurs while reading
+        return 0;
     }
-    printf("Encrypted message: %s %s %s", message[1], message[2], message[3] );
+    while(!feof(input)) { //this while loop scans all of input and prints every character until the end of file
+        fscanf(input, "%c", &c);
+        printf("%c", c);
+    }
     return 0;
 }
