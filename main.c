c5fb8096394534e6f0edf4a52e68f1d9414ca7b3
Jacob Mahony <c3330522@uon.edu.au>
2019-04-17 04:07:26 +0000

    rotation cipher decryption vfinal

--- main.c
+++ main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int encrypt(int x);
+int decrypt(int x);
 
 int main() {
-    char m[1000]; //creates an array which can store up to 1000 characters
-    char en[1000];
+    char c[1000]; //creates an array which can store up to 1000 characters
+    char de[1000];
     int i, j, n;
     FILE *input; //pointer to input
     input = fopen("input", "r"); //reads input
-    fscanf(input, " %[^\n]s", m); //scans the entirety of input and assigns it to 1 string
-    n=strlen(m);
-    printf("Original Message: %s len: %d\n", m, n);
-    /*for(i = 0; i<n; i++) { // m[i] != '\0' && en[i] != '\0' i accesses each character in message string
-        en[i] = encrypt(m[i]);
-        printf("\t%d, %c, %d, %c\n", en[i], en[i], m[i], m[i]);*/
+    fscanf(input, " %[^\n]s", c); //scans the entirety of input and assigns it to 1 string
+    n=strlen(c); //assigns the amount of characters in message to variable n
+    printf("Encrypted Message: %s len: %d\n", c, n);
+    for(i = 0; i<n; i++) { // m[i] != '\0' && en[i] != '\0' i accesses each character in message string
+        de[i] = decrypt(c[i]); //decrypts each character in string
+        //printf("\t%d, %c, %d, %c\n", en[i], en[i], m[i], m[i]);
     }
-    printf("Encrypted Message: %s", en); //prints the whole encrypted message
+    printf("Decrypted Message: %s", de); //prints the whole decrypted message
     return 0;
 }
 
-int encrypt(int x) { //encryption function definition
+int decrypt(int x) { //encryption function definition
     int key = 1;
     int e;
     if(x == 32 ) { //if the character is space, return a space
@@ -31,7 +31,7 @@ int encrypt(int x) { //encryption function definition
     }    
     else {
         x = x-65; //this shifts the ASCII number for the capital letter by 65 to make our equation below work
-    e = (x + key)%26 + 65; //shifts the letter by the given key and then adds 65 to return it to its proper ASCII code
+    e = (x - key)%26 + 65; //shifts the letter by the given key and then adds 65 to return it to its proper ASCII code
     return e;
 }
 }
