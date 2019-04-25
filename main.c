b94c60ceccafa3ecdc17643d701910e458f7c203
Jacob Mahony <c3330522@uon.edu.au>
2019-04-25 22:36:17 +0000

    decrypted unseen message with rotation cipher

--- main.c
+++ main.c
@@ -1,52 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
-//ABCDEFGHIJKLMNOPQRSTUVWXYZ
-//QWERTYUIOPASDFGHJKLZXCVBNM
-
-int decrypt(int x);
-
-int find_index(int match);
+int bruteForceDecrypt(int x);
 
 int main() {
-    char c[1000]; //array of length 1000 to hold message string
-    char de[1000];//array of length 1000 to hold decrypted message string
-    int i, len;
+    char message[1000]; //this array holds a message of up to 1000 characters
+    int i, j, k, l, len;
+    char check1[3] = {'T', 'H', 'E'}; //this array holds the string 'THE'. This will be the word that we check for in each possible decrypted message to see if it is actually decrypted
     FILE *input; //file pointer to input
     input = fopen("input", "r"); //opens input for reading
-    fscanf(input, " %[^\n]s", c); //scans all of input and stores it in c
-    len = strlen(c); //stores number of characters of the message in variable len
-    printf("Encrypted Message: %s %d\n", c, len);
-    for(i = 0; i < len; i++) {
-        de[i] = decrypt(c[i]); //decrypts original message
-        //printf("\t%d %c %d %c\n", c[i], c[i], de[i], de[i]); //prints ASCII value and character for the character in the original message and the encrypted message
+    fscanf(input, " %[^\n]s", message); //scans all of input and stores it in message
+    len = strlen(message); //stores length of string in len
+    printf("Encrypted Message: %s\n", message);
+    for(i = 0; i < 26; i++) { //this is our for loop that tests for each possible decrypted message
+        for(j = 0; j < len; j++) {
+            message[j] = bruteForceDecrypt(message[j]);
+        }
+        for(k = 0; k < len; k++) {
+            if(check1[0] == message[k]) { //this checks to see if a given letter in one of the possible decrypted messages is the character T.
+                if(check1[1] == message[k+1]) { //checks to see if the following character is H
+                    if(check1[2] == message[k+2]) { //checks to see if the following character is E
+                        printf("A proper word has been found, hence the cipher has been decrypted\n");
+                        printf("Original Message: %s", message);
+                        FILE *output; //file pointer to output
+                        output = fopen("output", "w"); //opens output for writing
+                        for(l = 0; l < len; l++) {
+                            fprintf(output, "%c", message[l]); //prints each individual character of the message to file output
+                        }
+                        return 0;
+                    }
+                }
+            }
+        }
     }
-    printf("Decrypted Message: %s\n", de);
-    return 0;
 }
 
-int decrypt(int x) {
-    int e;
-    x = x - 65; //this shifts the ASCII number for the capital letter by 65 to make our equation below work
+int bruteForceDecrypt(int x) {
+    int i, d;
+    x = x - 65; //makes our letters have a value between 0 and 25 (for A - Z)
     if(x >= 0 && x < 26) {
-        e = find_index(x);
-        e = e + 65; //shifts the number by 65 to make our character the correct ASCII number corresponding to the letter
-        return e;
-}
-    else {
+        d = ((x + 1) % 26) + 65; //shifts each character in the message by 1 and returns it to its original ASCII value
+        return d;
+    }
+    else { //if the character is a non-capital letter characterm this returns that character
         x = x + 65;
         return x;
     }
-}
-
-int find_index(int match) {
-    int j;
-    char enc_index[26] = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'}; //this string holds the value for each new letter in the index of the array
-    for(j = 0; j < 26; j++) {
-        enc_index[j] = enc_index[j] - 65;
-        if(enc_index[j] == match) { //when the character 'match' is the same as the character in the array enc_index, return the index corresponding to that character in the array
-            return j;
-        }
-    }
-    return 0;
 }
